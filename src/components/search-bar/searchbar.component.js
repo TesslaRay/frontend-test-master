b123b2517d6b586197ec4a9e7886eadc8f277cb4
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2021-01-05 21:36:40 -0300

    feature: component SearchBar

---
+++ src/components/search-bar/searchbar.component.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+// TODO: font-family
+const useStyles = makeStyles((theme) => ({
+  search: {
+    position: 'relative',
+    borderRadius: '8px',
+    backgroundColor: 'white',
+    '&:hover': {
+      border: `2px solid ${theme.palette.primary.main}`,
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    color: '#888B90',
+    boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.05)',
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#888B90',
+    fontSize: '16px',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          placeholder="Search Counters"
+          classes={{
+            input: classes.inputInput,
+          }}
+          inputProps={{'aria-label': 'search'}}
+        />
+      </div>
+    </Box>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search-bar/searchbar.component';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+
 // You don't have to use `fetch` btw, use whatever you want
-const getCounters = () => 
-  fetch('/api/v1/counter', { method: 'get' })
-    .then(res => res.json());
+const getCounters = () => fetch('/api/v1/counter', {method: 'get'}).then((res) => res.json());
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#FF9500',
+    },
+  },
+  typography: {
+    fontFamily: 'Avenir Next',
+    fontSize: 11,
+  },
+});
 
 const App = () => {
   React.useEffect(() => {
@@ -12,11 +25,16 @@ const App = () => {
   }, []);
 
   return (
-    <h1>Hello, Cornershop!</h1>
+    <div>
+      <SearchBar />
+    </div>
   );
 };
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+
+  document.querySelector('#root'),
 );
